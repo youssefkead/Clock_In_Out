224aa4ded998c268b571743a74290dd3b49b1928
youssefkead <105119228+youssefkead@users.noreply.github.com>
2024-04-18 05:23:27 +0200

    3rd commit from home

--- dumpintocsv.py
+++ dumpintocsv.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
+from datetime import datetime
+import os
+
+print(os.getcwd())
 
 root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
@@ -19,12 +24,15 @@ entry1["textvariable"] = contents
 def print_contents(event):
     print(contents.get())
 
-entry1.bind('<Key-Return>', print_contents)
+dict = {'text':[], 
+        'timestamp':[]}
+df = pd.DataFrame(dict)
 
-def print_it(event):
-    print("Works!")
+def append_it(*args):
+    df.loc[len(df.index)] = [contents.get(),datetime.now()]
 
-button2 = ttk.Button(frm,text="Submit",command=print_it, padding=10)
+entry1.bind('<Key-Return>', append_it)
+button2 = ttk.Button(frm,text="Submit",command=append_it, padding=10)
 button2.grid(column=1, row=5, pady=10)
 
 root.grid_rowconfigure(0, weight=1)
@@ -32,4 +40,24 @@ root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(4, weight=1)
 root.grid_columnconfigure(2, weight=1)
 
+#pandas and storing data in a csv
+
+c = datetime.now()
+current_time = c.strftime('%H:%M:%S')
+
+# storing the current time in the variable
+c2 = datetime.now().time()  # time object
+
+print("c is", c)
+print("c2 is", c2)
+
+# Displays Time
+print('Current Time is:', current_time)
+print('Type(c):', type(c))
+
 root.mainloop()
+
+print(df)
+path = "C:\\Users\\youssef\\Documents\\code_is_here"
+df.to_csv(os.path.join(path,r'text_and_timestamps.csv'), sep=',', index=False, encoding='utf-8')
+
