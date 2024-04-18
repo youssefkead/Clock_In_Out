bff2f70c8ae04a74c0166176a0f576d1f259f615
youssefkead <105119228+youssefkead@users.noreply.github.com>
2024-04-18 05:51:20 +0200

    4th commit

--- dumpintocsv.py
+++ dumpintocsv.py
@@ -8,7 +8,13 @@ print(os.getcwd())
 
 root = tk.Tk()
 frm = ttk.Frame(root, padding=10)
-frm.grid()
+frm.grid(row=1,column=1)
+
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(2, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(2, weight=1)
+
 label1 = ttk.Label(frm, text="Hello World!", padding=10)
 button1 = ttk.Button(frm, text="Quit", command=root.destroy, padding=2)
 
@@ -20,6 +26,12 @@ entry1.grid(column=1, row=3, pady=20)
 contents = tk.StringVar()
 contents.set("This is a variable")
 entry1["textvariable"] = contents
+
+def toggle(button: tk.Button):
+    if button.config('relief')[-1] == 'sunken':
+        button.config(relief="raised")
+    else:
+        button.config(relief="sunken")
 
 def print_contents(event):
     print(contents.get())
@@ -34,11 +46,6 @@ def append_it(*args):
 entry1.bind('<Key-Return>', append_it)
 button2 = ttk.Button(frm,text="Submit",command=append_it, padding=10)
 button2.grid(column=1, row=5, pady=10)
-
-root.grid_rowconfigure(0, weight=1)
-root.grid_columnconfigure(0, weight=1)
-root.grid_rowconfigure(4, weight=1)
-root.grid_columnconfigure(2, weight=1)
 
 #pandas and storing data in a csv
 
--- gui.py
+++ gui.py
@@ -1,24 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
-
 root = tk.Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-label1 = ttk.Label(frm, text="Hello World!")
-button1 = ttk.Button(frm, text="Quit", command=root.destroy)
 
-label1.grid(column=0, row=0)
-button1.grid(column=1, row=0)
+toggle_btn = tk.Button(text="Toggle", width=12, relief="raised", command=lambda:toggle(toggle_btn))
+toggle_btn.pack(pady=5)
 
-entry1 = tk.Entry()
-entry1.grid()
-contents = tk.StringVar()
-contents.set("This is a variable")
-entry1["textvariable"] = contents
+def toggle(button: tk.Button):
+    if button.config('relief')[-1] == 'sunken':
+        button.config(relief="raised")
+    else:
+        button.config(relief="sunken")
 
-def print_contents(event):
-    print(contents.get())
 
-entry1.bind('<Key-Return>', print_contents)
 
 root.mainloop()
